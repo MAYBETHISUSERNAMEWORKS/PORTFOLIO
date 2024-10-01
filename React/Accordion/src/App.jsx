36d7f2ba08f88809a2b5c8b1bf2305c38be1bd50
Marco Gomes <marco6_1@hotmail.com>
2024-10-01 16:58:36 +0100

    React App Accordian component

---
+++ React/Accordion/src/App.jsx
@@ -0,0 +1,13 @@
+import Accordian from "./assets/Components/Accordian/Accordian"
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Accordian />
+    </>
+  )
+}
+
+export default App
