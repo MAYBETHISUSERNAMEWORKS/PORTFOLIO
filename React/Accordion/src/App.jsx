92411fb2ae9dcf7c3882b24a9b4b4ec4ff8725ab
Marco Gomes <marco6_1@hotmail.com>
2024-10-12 18:30:40 +0100

    add LoadMoreData component

--- React/Accordion/src/App.jsx
+++ React/Accordion/src/App.jsx
@@ -1,7 +1,8 @@
 // import Accordian from "./assets/Components/Accordian/Accordian"
 // import RandomColor from "./assets/Components/Random-Color"
 // import StarRating from "./assets/Components/star-ratings"
-import ImageSlider from "./assets/Components/Image-slider"
+// import ImageSlider from "./assets/Components/Image-slider"
+import LoadMoreData from "./assets/Components/load-more-data"
 
 function App() {
 
@@ -10,11 +11,12 @@ function App() {
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
       {/* <StarRating noOfStars={10} /> */}
-      <ImageSlider
+      {/* <ImageSlider
          url={"https://picsum.photos/v2/list"}
          page={1}
          limit={10}
-      />
+      /> */}
+      <LoadMoreData />
     </>
   )
 }
